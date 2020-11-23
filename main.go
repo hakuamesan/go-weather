6be0fa46c852d87601077154b2c93d1ce550aeac
hakuamesan <hakuame@gmail.com>
2020-11-23 20:06:12 +0000

    updated var names n display

--- main.go
+++ main.go
@@ -9,43 +9,42 @@ import (
 	"time"
 )
 
-
 type WeatherResponse struct {
-		Coord struct {
-			Lon float64
-			Lat float64
-		}
-		Main struct {
-			Temp       float64
-			Feels_like float64
-			Temp_Min   float64
-			Temp_Max   float64
-			Humidity   float64
-			Pressure   float64
-		}
-		Visibility float64
-		Weather    []struct {
-			Main        string
-			Description string
-			Icon        string
-		}
-
-		Wind struct {
-			Speed float64
-			Deg   float64
-		}
-		Dt  float64
-		Sys struct {
-			Country string
-			Sunrise float64
-			Sunset  float64
-		}
-		Timezone float64
-		Clouds   struct {
-			All float64
-		}
-		Name string
+	Coord struct {
+		Lon float64
+		Lat float64
 	}
+	Main struct {
+		Temp       float64
+		Feels_like float64
+		Temp_Min   float64
+		Temp_Max   float64
+		Humidity   float64
+		Pressure   float64
+	}
+	Visibility float64
+	Weather    []struct {
+		Main        string
+		Description string
+		Icon        string
+	}
+
+	Wind struct {
+		Speed float64
+		Deg   float64
+	}
+	Dt  float64
+	Sys struct {
+		Country string
+		Sunrise float64
+		Sunset  float64
+	}
+	Timezone float64
+	Clouds   struct {
+		All float64
+	}
+	Name string
+}
 
 func main() {
 	var (
@@ -95,26 +94,24 @@ func main() {
 		fmt.Println(string(data))
 	}
 
-	
-
 	var f WeatherResponse
 	err = json.Unmarshal(data, &f)
 	if err != nil {
 		fmt.Printf("Error unmarshall %s", err)
 	}
 
-	fmt.Printf("Current weather at %s, %s is:\n", f.Name, f.Sys.Country)
-	fmt.Printf("Latitude: %.2f, Longitude: %.2f \n", f.Coord.Lat, f.Coord.Lon)
-	fmt.Printf("Current temperature is %.2f%s \n", f.Main.Temp, degree)
-	fmt.Printf("Feels like %.2f%s \n", f.Main.Feels_like, degree)
-	fmt.Printf("Description: %s \n", f.Weather[0].Description)
-	fmt.Printf("Wind Speed %.2f%s \n", f.Wind.Speed, speed)
-	fmt.Printf("Clouds %.2f \n", f.Clouds.All)
-	fmt.Printf("Visibility %.2f \n", f.Visibility)
-	sr := time.Unix(int64(f.Sys.Sunrise), 0)
-	ss := time.Unix(int64(f.Sys.Sunset), 0)
+	fmt.Printf("Current weather at %s, %s:\n", f.Name, f.Sys.Country)
+	fmt.Printf("\t Latitude: %.2f, Longitude: %.2f \n", f.Coord.Lat, f.Coord.Lon)
+	fmt.Printf("\t Current temperature is %.2f%s \n", f.Main.Temp, degree)
+	fmt.Printf("\t Feels like %.2f%s \n", f.Main.Feels_like, degree)
+	fmt.Printf("\t Description: %s \n", f.Weather[0].Description)
+	fmt.Printf("\t Wind Speed %.2f%s \n", f.Wind.Speed, speed)
+	fmt.Printf("\t Clouds %.2f \n", f.Clouds.All)
+	fmt.Printf("\t Visibility %.2f \n", f.Visibility)
+	sunrise := time.Unix(int64(f.Sys.Sunrise), 0)
+	sunset := time.Unix(int64(f.Sys.Sunset), 0)
 	fmt.Printf("Current time is %s\n", time.Now().Format("3:05PM"))
-	fmt.Printf("Sunrise is at %d:%d:%d \n", sr.Hour(), sr.Minute(), sr.Second())
-	fmt.Printf("Sunset is at %d:%d:%d \n", ss.Hour(), ss.Minute(), ss.Second())
+	fmt.Printf("Sunrise is at %d:%d:%d \n", sunrise.Hour(), sunrise.Minute(), sunrise.Second())
+	fmt.Printf("Sunset is at %d:%d:%d \n", sunset.Hour(), sunset.Minute(), sunset.Second())
 
 }
